09e82a7276d66a33fea8d0bc55b353e4c2581ff2
tahasamad5 <52231356@students.liu.edu.lb>
2025-11-23 19:03:18 +0200

    Create jason data for book

---
+++ src/data/books.js
@@ -0,0 +1,69 @@
+export const booksData = [
+  {
+    id: 1,
+    title: "The House of Hilltop",
+    author: "L.A. Shute ",
+    price: 12.99,
+    category: "Fiction",
+    image: "/images/books/picture2.jpg",
+    description: "A classic American novel set in the Jazz Age, exploring themes of wealth, love, and the American Dream.",
+    rating: 4.5,
+    stock: 15
+  },
+  {
+    id: 2,
+    title: "The Old Man and Sea",
+    author: "Ernest Hemingway",
+    price: 14.99,
+    category: "Fiction",
+    image: "/images/books/picture3.jpg",
+    description: "A gripping tale of racial injustice and childhood innocence in the American South.",
+    rating: 4.8,
+    stock: 20
+  },
+  {
+    id: 3,
+    title: "The First Ladies",
+    author: "Margret Truman",
+    price: 13.99,
+    category: "Science Fiction",
+    image: "/images/books/picture1.jpg",
+    description: "A dystopian social science fiction novel and cautionary tale about totalitarianism.",
+    rating: 4.7,
+    stock: 18
+  },
+  {
+    id: 4,
+    title: "The Dragon Keeper",
+    author: "Robine Hobb",
+    price: 11.99,
+    category: "Fantasy",
+    image: "/images/books/picture4.jpg",
+    description: "A romantic novel of manners that charts the emotional development of the protagonist.",
+    rating: 4.6,
+    stock: 12
+  },
+  {
+    id: 5,
+    title: "A Part Of You",
+    author: "Lyden Renwick",
+    price: 13.49,
+    category: "Romance",
+    image: "/images/books/Picture5.jpg",
+    description: "A story about teenage rebellion and angst, narrated by the cynical Holden Caulfield.",
+    rating: 4.2,
+    stock: 10
+  },
+  {
+    id: 6,
+    title: "Children Of Time",
+    author: "Clarke Award",
+    price: 15.99,
+    category: "Sience Fiction",
+    image: "/images/books/Picture8.jpg",
+    description: "The first book in the Harry Potter series, introducing the magical world of Hogwarts.",
+    rating: 4.9,
+    stock: 25
+  },
+  
+];
